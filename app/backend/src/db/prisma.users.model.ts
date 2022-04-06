ebf944c0ae3bb3020f306d1d39ac894695695425
MuriloM <muriloommaia@gmail.com>
2022-04-05 23:46:00 -0300

    feat: ajust prisma

--- app/backend/src/db/prisma.users.model.ts
+++ app/backend/src/db/prisma.users.model.ts
@@ -1,6 +1,18 @@
+import { Entity, User } from '../app/domain'
 import { prisma } from './client'
 export class PrismaUsersModel {
   async getAllUsers(): Promise<any> {
     return await prisma.user.findMany()
+  }
+
+  async createUser(user: Omit<User, keyof Entity>): Promise<any> {
+    const { name, email, password } = user
+    return await prisma.user.create({
+      data: {
+        name,
+        email,
+        password
+      }
+    })
   }
 }
