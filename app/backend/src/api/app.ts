a279439f6825a84a8b40452304e8a55db3af905b
MuriloM <muriloommaia@gmail.com>
2022-04-06 20:02:18 -0300

    feat: ajust cors, ajust eslint

--- app/backend/src/api/app.ts
+++ app/backend/src/api/app.ts
@@ -1,13 +1,15 @@
+import cors from 'cors'
 import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import { errorMiddleware } from './middleware'
 import { userRoute } from './routes/userRoute'
 
-
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/api/users', userRoute)
 
