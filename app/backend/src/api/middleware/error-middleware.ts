fc358804f84f839f3ea376a7625292d648155a69
MuriloM <muriloommaia@gmail.com>
2022-04-06 00:30:43 -0300

    feat: increment unauthorized error

--- app/backend/src/api/middleware/error-middleware.ts
+++ app/backend/src/api/middleware/error-middleware.ts
@@ -2,10 +2,11 @@ import { NextFunction, Request, Response } from 'express'
 
 const errorMiddleware = (err: Error, _req: Request, res: Response, next: NextFunction): any => {
   const { name, message } = err
-  console.log(name)
+  console.log(err)
   switch (name) {
     case 'BadRequestError': return res.status(400).json({ message })
     case 'ValidationError': return res.status(401).json({ message })
+    case 'UnauthorizedError': return res.status(401).json({ message })
     default:
       res.status(500).json({
         message: 'Something went wrong'
