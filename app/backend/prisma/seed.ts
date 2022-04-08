90d44a618ddd2850567a3271b2619e9c9d3e11b8
MuriloM <muriloommaia@gmail.com>
2022-04-08 00:21:20 -0300

    feat: atualiza seeders

--- app/backend/prisma/seed.ts
+++ app/backend/prisma/seed.ts
@@ -24,6 +24,46 @@ const usersMap = [
     name: 'Michael Caxias',
     email: 'micha@teste.com',
     password: '$2a$10$wEXLgVq.pwsM1eoiAER91eNhVx4ViPlNw3XwfWNZgXjhl7t//el32'
+  },
+  {
+    name: 'Joana Andrade',
+    email: 'andradejoana@hotmal.com',
+    password: '$2a$10$ecpU91vU02C.K66SIDuosuph1oW4XExd7zMuUxwF1CpNJXdVVbxN.'
+  },
+  {
+    name: 'Herbert Santos',
+    email: 'herbert@teste.com',
+    password: '$2a$10$wN/xnvY/VTnLXE3g2EKZG.CcPyTuYuC6jkpUbtDyGmhlOwXHcE9G6'
+  },
+  {
+    name: 'Carolina Silva',
+    email: 'carolinasilva@uol.com',
+    password: '$2a$10$vGh2zP8701d27uj42pcH9.H7vUsuTBQSGw2c9NFxokn7c/fzkfzOS'
+  },
+  {
+    name: 'Caetano Caxias',
+    email: 'caca@teste.com',
+    password: '$2a$10$wEXLgVq.pwsM1eoiAER91eNhVx4ViPlNw3XwfWNZgXjhl7t//el32'
+  },
+  {
+    name: 'Fernanda Vasconcelos',
+    email: 'fevas@bing.com',
+    password: '$2a$10$ecpU91vU02C.K66SIDuosuph1oW4XExd7zMuUxwF1CpNJXdVVbxN.'
+  },
+  {
+    name: 'Daiane Ferreira',
+    email: 'daiane@teste.com',
+    password: '$2a$10$wN/xnvY/VTnLXE3g2EKZG.CcPyTuYuC6jkpUbtDyGmhlOwXHcE9G6'
+  },
+  {
+    name: 'Martin Fernando',
+    email: 'martin@hotmail.com',
+    password: '$2a$10$vGh2zP8701d27uj42pcH9.H7vUsuTBQSGw2c9NFxokn7c/fzkfzOS'
+  },
+  {
+    name: 'Hellen Caxias',
+    email: 'hellen@teste.com',
+    password: '$2a$10$wEXLgVq.pwsM1eoiAER91eNhVx4ViPlNw3XwfWNZgXjhl7t//el32'
   }
 ]
 
