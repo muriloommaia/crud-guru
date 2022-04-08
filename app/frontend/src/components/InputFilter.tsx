3ec50a25263d7fdfe66fadfed1adde57f2579c39
MuriloM <muriloommaia@gmail.com>
2022-04-07 22:57:10 -0300

    feat: style input

--- app/frontend/src/components/InputFilter.tsx
+++ app/frontend/src/components/InputFilter.tsx
@@ -12,16 +12,22 @@ export default function InputFilter() {
     dispatch(setPage(1));
   };
   return (
-    <div className="flex items-center">
-      <div className="bg-grey col-12 mt-3 align-middle justify-content-center flex">
-        <input type="search" className="col-8 border-2 p-2 rounded-xl text-center" placeholder="Search for name or email" id="search-filter" name="filter" onChange={eventChange} value={filter} />
-      </div>
-      <div className="text-stone-50">
-        <p>
-          {total}
+    <div className="flex w-11/12 items-center justify-between mb-4">
+      <div className="text-stone-50 flex">
+        <p className="">
           <i className="fa-solid fa-users" />
         </p>
+        <p className="mx-2">
+          {total}
+          {' '}
+          resultados
+        </p>
       </div>
+      <div className="bg-grey col-12 align-middle justify-content-center flex items-center col-8 border-2 p-2 rounded-xl text-center bg-slate-50">
+        <input type="search" className=" focus:outline-none" placeholder="Pesquisar nome ou email" id="search-filter" name="filter" onChange={eventChange} value={filter} />
+        <i className="fa-solid fa-magnifying-glass" />
+      </div>
+
     </div>
   );
 }
